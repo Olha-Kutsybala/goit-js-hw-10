3243a666331a0343ae1aa6e54c175b5a51c2cf5b
Olha-Kutsybala <olyasheremeta@gmail.com>
2023-04-17 17:10:17 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -4,7 +4,7 @@ import Notiflix from 'notiflix';
 
 export function fetchCountries(name) {
   return fetch(
-    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,language`
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (!response.ok) {
       throw new Error(
